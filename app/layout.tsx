28cef27589753d356cddfdeb1b8e95fcea9e844c
Soham Tamboli <sohamtamboli@gmail.com>
2022-11-15 04:28:48 +0530

    basic flexbox layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,30 @@
-import './globals.css'
+import { Roboto } from "@next/font/google";
+import "../styles/globals.scss";
+import styles from "../styles/layout.module.scss";
+
+const roboto = Roboto({
+	weight: ["400", "500", "700"],
+});
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      {/*
+	return (
+		<html lang='en' className={roboto.className}>
+			{/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
-      <head />
-      <body>{children}</body>
-    </html>
-  )
+			<head />
+			<body className={styles.mainFlexLayout}>
+				<div className={styles.leftSidebar}>{/* sidebar */}left side</div>
+				<div className={styles.rightContent}>
+					{/* main content */}
+					{children}
+				</div>
+			</body>
+		</html>
+	);
 }
